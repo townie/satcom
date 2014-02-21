8db990c1b77ca66865858783fff9ae8a15c03c1f
Keith Webber <keith.e.webber@gmail.com>
2014-02-21 18:48:05 -0500

    START OF Satcom build off of Treebook

---
+++ app/models/user.rb
@@ -0,0 +1,28 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # attr_accessible :email, :password, :password_confirmation,
+  #                 :first_name, :last_name, :profile, :remember_me
+
+
+  has_many :statuses
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  # validates :profile, presence: true,
+  #    uniqueness: true,
+  #    format: {
+  #       with: /a-zA-Z0-9/,
+  #       message: 'Must be formated correctly.'
+  #    }
+
+
+
+  def full_name
+    first_name + " " + last_name
+  end
+
+end
